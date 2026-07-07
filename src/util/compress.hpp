b12d6b646bcab1442333918ad572ee624d935610
grazaerd <dnblank143@gmail.com>
2026-07-07 11:02:45 +0800

    Update compress

--- src/util/compress.hpp
+++ src/util/compress.hpp
@@ -6,22 +6,17 @@
 
 #include "file_input.hpp"
 #include "libdeflate.h"
-#include "../pak/pak_struct.hpp"
 
 class compress {
 public:
-    // TODO: delete other function; rule of 5
-    compress(int compress_level, const char* path);
+    compress(int compress_level);
     
-    ~compress();
-    
-    bool work(unsigned char** out, uint64_t* outsz, const char* path) ;
+    bool work(unsigned char** out, uint64_t* outsz,  file_in& path) ;
     
 private:
     libdeflate_compressor* compressor;
     std::vector<unsigned char*> compressedfile {};
     uint64_t zlib_bound;
-    file_in inpath;
 };
 
 #endif
